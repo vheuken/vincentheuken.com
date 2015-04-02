07a54044aa01f9a0f5473033705e8289ba334fd4
Vincent Heuken <me@vincentheuken.com>
2015-04-02 02:27:56 -0700

    index.php now has title and subtitle fetched from wordpress

--- header.php
+++ header.php
@@ -1,2 +1,17 @@
 <html>
-  TEST!
+
+<head>
+  <title>Test Title!</title>
+
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+
+<div id="header">
+  <div id="title">
+    <?php echo get_bloginfo('name') ?>
+  </div>
+
+  <div id="subtitle">
+    <?php echo get_bloginfo('description') ?>
+  </div>
+</div>
