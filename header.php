348734c7dc3b73ce2410cf51a5644d53e22ffef2
Vincent Heuken <me@vincentheuken.com>
2015-04-02 02:54:08 -0700

    added JS

--- header.php
+++ header.php
@@ -4,6 +4,9 @@
   <title><?php echo get_bloginfo('name') ?></title>
 
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+  <script type="text/javascript" src="jquery.ba-hashchange.min.js"></script>
+  <script type="text/javascript" src=<?php echo get_template_directory_uri() . "/script.js"; ?>></script>
 </head>
 
 <body>
