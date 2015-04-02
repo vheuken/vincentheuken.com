43561f66c74dbc93194cbfbdeb82e03b73d8b4d2
Vincent Heuken <me@vincentheuken.com>
2015-04-02 02:45:02 -0700

    added content and some filler

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php get_header(); ?>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
+
+<div id="content">
+  <div id="about">
+    <p>
+      This is just some filler!<br>
+      Lorem ipsum etc<br>
+    </p>
+  </div>
+</div>
