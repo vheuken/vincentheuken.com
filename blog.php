0aa16c5eadb533382fb0bde5ee81857d73c4ec55
Vincent Heuken <me@vincentheuken.com>
2015-04-05 23:31:31 -0700

    added content preview

--- blog.php
+++ blog.php
@@ -1,5 +1,6 @@
 <?php while (have_posts()) : the_post(); ?>
   <h1><?php the_title(); ?></h1>
   <h4>Posted on <?php the_time('F jS, Y'); ?> </h4>
+  <p><?php the_content(__('(more...)')); ?></p>
 <?php endwhile; ?>
 
