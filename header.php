7430049cfbac1d1b3fd83863968cd7552c4d14bb
Vincent Heuken <me@vincentheuken.com>
2015-04-06 14:30:20 -0700

    removed unused css file

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
   <title><?php echo get_bloginfo('name') ?></title>
 
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-  <link rel="stylesheet" href=<?php echo get_template_directory_uri() . "/css/rrssb.css"; ?>>
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
   <script type="text/javascript" src="jquery.ba-hashchange.min.js"></script>
   <script type="text/javascript" src=<?php echo get_template_directory_uri() . "/script.js"; ?>></script>
