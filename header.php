15a9a79d2763c10e987944fad570914e7e8fe463
Vincent Heuken <me@vincentheuken.com>
2015-04-06 14:19:19 -0700

    added social buttons

--- header.php
+++ header.php
@@ -23,15 +23,17 @@
 
   <div id="social_buttons">
     <span class="rrssb-icon">
-      <?php echo file_get_contents(get_template_directory_uri() . "/icons/mail.svg"); ?>
+      <a class="image_link" href="mailto:me@vincentheuken.com"><?php echo file_get_contents(get_template_directory_uri() . "/icons/mail.svg"); ?>
+    </a>
     </span>
 
     <span class="rrssb-icon">
-      <?php echo file_get_contents(get_template_directory_uri() . "/icons/github.svg"); ?>
+      <a class="image_link" href="github.com/vheuken"><?php echo file_get_contents(get_template_directory_uri() . "/icons/github.svg"); ?></a>
     </span>
 
     <span class="rrssb-icon">
-      <?php echo file_get_contents(get_template_directory_uri() . "/icons/linkedin.svg"); ?>
+      <a class="image_link" href="https://www.linkedin.com/pub/vincent-heuken/26/453/366"><?php echo file_get_contents(get_template_directory_uri() . "/icons/linkedin.svg"); ?>
+      </a>
     </span>
   </div>
 </div>
