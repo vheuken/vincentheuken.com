3bc0f6609c0268dd29a622e8b6210a8ef5446de7
Vincent Heuken <me@vincentheuken.com>
2015-04-06 13:53:04 -0700

    added social buttons

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
   <title><?php echo get_bloginfo('name') ?></title>
 
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+  <link rel="stylesheet" href=<?php echo get_template_directory_uri() . "/css/rrssb.css"; ?>>
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
   <script type="text/javascript" src="jquery.ba-hashchange.min.js"></script>
   <script type="text/javascript" src=<?php echo get_template_directory_uri() . "/script.js"; ?>></script>
@@ -19,14 +20,19 @@
   <div id="subtitle">
     <?php echo get_bloginfo('description') ?>
   </div>
-</div>
 
-<div id="social_buttons">
-  <div class="social_button" id="email_button">
-  </div>
-  <div class="social_button" id="github_button">
-  </div>
-  <div class="social_button" id="twitter_button">
+  <div id="social_buttons">
+    <span class="rrssb-icon">
+      <?php echo file_get_contents(get_template_directory_uri() . "/icons/mail.svg"); ?>
+    </span>
+
+    <span class="rrssb-icon">
+      <?php echo file_get_contents(get_template_directory_uri() . "/icons/github.svg"); ?>
+    </span>
+
+    <span class="rrssb-icon">
+      <?php echo file_get_contents(get_template_directory_uri() . "/icons/linkedin.svg"); ?>
+    </span>
   </div>
 </div>
 
