43d130d27c904d5c2595c2b5a903493129fc5f56
Vincent Heuken <me@vincentheuken.com>
2015-04-06 15:06:11 -0700

    now blog only shows preview and links to post

--- blog.php
+++ blog.php
@@ -1,6 +1,7 @@
 <?php while (have_posts()) : the_post(); ?>
   <h1><?php the_title(); ?></h1>
   <h4>Posted on <?php the_time('F jS, Y'); ?> </h4>
-  <p><?php the_content(); ?></p>
+  <p><?php the_excerpt(); ?></p>
+  <a href="<?php echo get_permalink(); ?>">Read more...</a>
 <?php endwhile; ?>
 
