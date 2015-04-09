14d810903f6e47e02677bbce342d625c985a2973
Vincent Heuken <me@vincentheuken.com>
2015-04-08 23:28:55 -0700

    added more stuff to single.php

--- single.php
+++ single.php
@@ -1,1 +1,17 @@
 <?php get_header(); ?>
+
+<div id="content">
+  <div id="post_content">
+   <p>
+   </p>
+  </div>
+
+  <div id="contact">
+    <p>
+      <?php echo get_page_by_title('Contact')->post_content; ?>
+    </p>
+  </div>
+</div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
