2e464cfa80e9e88d1e6e1559c862b2c077bc513e
Vincent Heuken <me@vincentheuken.com>
2015-04-10 14:22:23 -0700

    single.php now shows content

--- single.php
+++ single.php
@@ -3,6 +3,11 @@
 <div id="content">
   <div id="post_content">
    <p>
+    <?php while (have_posts()) : the_post(); ?>
+      <h1><?php the_title(); ?></h1>
+      <h4>Posted on <?php the_time('F jS, Y'); ?></h4>
+      <?php the_content(); ?>
+    <?php endwhile; ?>
    </p>
   </div>
 
