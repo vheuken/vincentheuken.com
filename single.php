0aaffbd6167b00e2be8dddb38d89d7c47b17c030
Vincent Heuken <me@vincentheuken.com>
2015-04-11 23:31:59 -0700

    added license to blogposts

--- single.php
+++ single.php
@@ -8,7 +8,8 @@
       <h4>Posted on <?php the_time('F jS, Y'); ?></h4>
       <?php the_content(); ?>
       <!-- Yes, I know the date is posted twice. This is intentional -->
-      <h4>Posted on <?php the_time('F jS, Y'); ?></h4>
+
+      <a rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nd/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/">Creative Commons Attribution-NoDerivatives 4.0 International License</a>.
     <?php endwhile; ?>
    </p>
   </div>
